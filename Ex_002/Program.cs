9e20e628d127d42747f7831f8cc03534552103dd
Svetlana <svdorohova@gmail.com>
2022-08-16 11:01:44 +0300

    search max

---
+++ Ex_002/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число");
+int q = Convert.ToInt32(Console.ReadLine());
+int max = a;
+
+if (max > b)
+{
+   max = a;
+}
+else
+{
+    max = b;
+}
+if (max < q)
+{
+  max = q;
+}
+Console.WriteLine($"Наибольшее число из введенных {max}");
+
