ce0b8f212406f88b1152a77ee58e414d5812ddda
Svetlana <svdorohova@gmail.com>
2022-08-16 11:25:05 +0300

    div  %2

---
+++ Ex_003/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 2;
+if (a % b == 0)
+{
+    Console.WriteLine($"Введенное число {a} четное");
+}
+else
+{
+    Console.WriteLine($"Введенное число {a} нечетное");
+}
